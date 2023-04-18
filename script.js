e71231ddd0f98f03951c37e0df81f5f383423dc4
Christian Morales <christianm9916@gmail.com>
2023-04-17 21:40:45 -0500

    proyecto finalizado

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const cc = document.querySelector("#checkT");
-console.log(cc.value);
+const cuenta = document.querySelector("#checkT");
+const tip = document.querySelector("#tip");
+
+const calculateTip = () => {
+    const resultadoTip = (cuenta.value * tip.value) / 100
+    document.querySelector("#tipD").value = resultadoTip;
+
+    document.querySelector("#totalB").value = parseInt(cuenta.value) + parseInt(resultadoTip);
+}
+
+cuenta.addEventListener("change", calculateTip);
+
+tip.addEventListener("change", calculateTip);
+
+
 
 
 
